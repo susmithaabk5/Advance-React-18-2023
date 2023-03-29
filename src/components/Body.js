0ee26d114b4528ff510ad39a8df7d203f2c62d91
susmithaa <susmithaajob@gmail.com>
2023-03-29 04:17:19 -0700

    shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { RestaurantCard } from "./RestaurantCard";
 import { restarauntList } from "../data/constatnt";
+import Shimmer from "./Shimmer";
 function filterData(searchInput, restarauntList) {
   const filterData = restarauntList.filter((restaurant) =>
     restaurant.data.name.toLowerCase().includes(searchInput.toLowerCase())
   );
   return filterData;
 }
+
 export const Body = () => {
   const [searchInput, setsearchInput] = useState("");
-  const [filteredRestaurants, setfilteredRestaurants] =
-    useState(restarauntList);
+  const [filteredRestaurants, setfilteredRestaurants] = useState([]);
   const onChangeInput = (e) => {
     setsearchInput(e.target.value);
   };
   const onSearchClick = () => {
-    debugger;
     const data = filterData(searchInput, restarauntList);
     setfilteredRestaurants(data);
   };
-  return (
+  useEffect(() => {
+    getRestaraunt();
+  }, []);
+  async function getRestaraunt() {
+    const data = await fetch(
+      " https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.1487898&lng=80.2305586&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json, "json```````````");
+    setfilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  return filteredRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="search-container">
         <input
---
+++ src/components/Shimmer.js
@@ -0,0 +1,9 @@
+const Shimmer = () => {
+  debugger;
+  return (
+    <>
+      <h1>Loading......</h1>
+    </>
+  );
+};
+export default Shimmer;
